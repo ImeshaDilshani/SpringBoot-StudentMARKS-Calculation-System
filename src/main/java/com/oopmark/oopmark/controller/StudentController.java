f9eaa6a0cf9dbe28607f0ed64f05e4dd50fc2eb1
ImeshaDilshani <imeshadilshani212@gmail.com>
2023-08-12 23:48:53 +0530

    connect to springboot API to FrontEnd

--- src/main/java/com/oopmark/oopmark/controller/StudentController.java
+++ src/main/java/com/oopmark/oopmark/controller/StudentController.java
@@ -2,26 +2,38 @@ package com.oopmark.oopmark.controller;
 
 import com.oopmark.oopmark.dto.StudentDTO;
 import com.oopmark.oopmark.service.StudentService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController
+@Controller
 @RequestMapping(value = "/student")
 @CrossOrigin
+@RequiredArgsConstructor
 public class StudentController {
 
     @Autowired
     private StudentService studentService;
+
+    @GetMapping("/insert")
+    public String save(){
+        return "insert";
+    }
     @PostMapping("/saveStudent")
     public StudentDTO saveStudent(@RequestBody StudentDTO studentDTO){
         return studentService.saveStudent(studentDTO);
     }
 
-
+    @GetMapping("/view")
+    public String view(){
+        return "view";
+    }
     @GetMapping("getStudent")
     public List<StudentDTO> getStudent(){
         return studentService.getAllStudent();
     }
+
 }
